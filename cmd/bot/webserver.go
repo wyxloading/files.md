8677a6ef6bbd36a554a404f6ea74e87b257cebf9
Artem Zakirullin <artemzr@gmail.com>
2024-11-13 12:05:34 +0200

    add app without trailing slash

--- cmd/bot/webserver.go
+++ cmd/bot/webserver.go
@@ -98,12 +98,15 @@ func habitsServer(habitsHost, certDir, logFilename string) {
 
 func setupRouter(router *http.ServeMux, logger *log.Logger) {
 	router.HandleFunc("/app/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/app" || r.URL.Path == "/app/" {
+		if r.URL.Path == "/app/" {
 			http.ServeFile(w, r, "./editor/editor.html")
 			return
 		}
 
 		http.StripPrefix("/app", http.FileServer(http.Dir("./editor"))).ServeHTTP(w, r)
+	})
+	router.HandleFunc("/app", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./editor/editor.html")
 	})
 
 	// TODO add hashing or secrets

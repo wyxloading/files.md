50ad7ca3362785f452608c904d8613818599460d
Artem Zakirullin <artemzr@gmail.com>
2024-12-09 13:16:12 +0200

    fix refactoring files app

--- web/app.js
+++ web/app.js
@@ -1,8 +1,8 @@
-let editor = null;
 let files = [];
+let editor = null;
 let focusedItemIndex = -1;
 
-function initHyperMD(el) {
+function initEditor(el) {
     editor = HyperMD.fromTextArea(el, {
         mode: "hypermd",
         lineNumbers: false,
@@ -23,13 +23,6 @@ function initHyperMD(el) {
         }
     });
     editor.setSize(null, "100%");
-
-    editor.addKeyMap({
-        'Cmd-Y': function (cm) {
-            cm.replaceSelection('✅ ');
-            cm.focus();
-        }
-    });
 
     editor.hmdResolveURL = function (path) {
         if (typeof path === 'undefined') {
@@ -56,6 +49,7 @@ function initHyperMD(el) {
 
         return path;
     };
+
     editor.hmdReadLink = async function (path) {
         path = path.replace('[', '').replace(']', '');
         let parts = path.split('/');
@@ -66,6 +60,7 @@ function initHyperMD(el) {
 
         await showFile(parts[0], parts[1] + '.md');
     };
+
     editor.on("inputRead", async function (cm, change) {
         if (change.text.length === 1 && change.text[0] === '[') {
             editor.showHint({
@@ -81,7 +76,10 @@ function initHyperMD(el) {
             await saveFile();
         }
     });
+
     editor.setOption("viewportMargin", Infinity);
+
+    // TODO Image uploading
     editor.on("paste", async (_, event) => {
         const items = (event.clipboardData || event.originalEvent.clipboardData).items;
         for (const item of items) {
@@ -103,7 +101,12 @@ function initHyperMD(el) {
             }
         }
     });
+
     editor.addKeyMap({
+        'Cmd-Y': function (cm) {
+            cm.replaceSelection('✅ ');
+            cm.focus();
+        },
         'Cmd-B': function (cm) {
             const selectedText = cm.getSelection();
             const isBold = selectedText.startsWith("**") && selectedText.endsWith("**");
@@ -388,7 +391,7 @@ async function getSavedDirectoryHandle() {
 }
 
 async function init(el) {
-    initHyperMD(el);
+    initEditor(el);
     buildSidebar();
     const savedDirectoryHandle = await getSavedDirectoryHandle();
     if (savedDirectoryHandle instanceof FileSystemDirectoryHandle) {
@@ -421,12 +424,6 @@ function openSearchModal() {
     const inputField = document.getElementById('goToFileInput');
     inputField.focus();
 
-    files = Object.keys(zettel)
-        .filter(folder => folder !== 'img')
-        .flatMap(folder =>
-            Object.keys(zettel[folder]).map(filename => ({folder, filename, lastModified: zettel[folder][filename].lastModified,}))
-        );
-    console.log(files);
     focusedItemIndex = -1;
     const goToFileResults = document.getElementById('goToFileResults');
     goToFileResults.innerHTML = '';
@@ -451,115 +448,18 @@ function closeSearchModal() {
     document.getElementById('goToFile').style.display = 'none';
 }
 
-function similarity(s1, s2) {
-    const distance = levenshtein(s1.toLowerCase(), s2.toLowerCase());
-    const maxLength = Math.max(s1.length, s2.length);
-
-    if (maxLength === 0) return 100;
-
-    const similarity = (1 - distance / maxLength) * 100;
-
-    return similarity.toFixed(2);
-}
-
-function levenshtein(s1, s2) {
-    if (s1 === s2) {
-        return 0;
-    }
-    var n = s1.length, m = s2.length;
-    if (n === 0 || m === 0) {
-        return n + m;
-    }
-    var x = 0, y, a, b, c, d, g, h, k;
-    var p = new Array(n);
-    for (y = 0; y < n;) {
-        p[y] = ++y;
-    }
-
-    for (; (x + 3) < m; x += 4) {
-        var e1 = s2.charCodeAt(x);
-        var e2 = s2.charCodeAt(x + 1);
-        var e3 = s2.charCodeAt(x + 2);
-        var e4 = s2.charCodeAt(x + 3);
-        c = x;
-        b = x + 1;
-        d = x + 2;
-        g = x + 3;
-        h = x + 4;
-        for (y = 0; y < n; y++) {
-            k = s1.charCodeAt(y);
-            a = p[y];
-            if (a < c || b < c) {
-                c = (a > b ? b + 1 : a + 1);
-            } else {
-                if (e1 !== k) {
-                    c++;
-                }
-            }
-
-            if (c < b || d < b) {
-                b = (c > d ? d + 1 : c + 1);
-            } else {
-                if (e2 !== k) {
-                    b++;
-                }
-            }
-
-            if (b < d || g < d) {
-                d = (b > g ? g + 1 : b + 1);
-            } else {
-                if (e3 !== k) {
-                    d++;
-                }
-            }
-
-            if (d < g || h < g) {
-                g = (d > h ? h + 1 : d + 1);
-            } else {
-                if (e4 !== k) {
-                    g++;
-                }
-            }
-            p[y] = h = g;
-            g = d;
-            d = b;
-            b = c;
-            c = a;
-        }
-    }
-
-    for (; x < m;) {
-        var e = s2.charCodeAt(x);
-        c = x;
-        d = ++x;
-        for (y = 0; y < n; y++) {
-            a = p[y];
-            if (a < c || d < c) {
-                d = (a > d ? d + 1 : a + 1);
-            } else {
-                if (e !== s1.charCodeAt(y)) {
-                    d = c + 1;
-                } else {
-                    d = c;
-                }
-            }
-            p[y] = d;
-            c = a;
-        }
-        h = d;
-    }
-
-    return h;
-}
-
 function loadRecentFiles() {
     const ignoredDirs = ["archive", "_read_", "_watch_", "_shop_", "habits", "triggers", "journal", "today", "later", "insights"];
+    let searchableFiles = Object.keys(zettel)
+        .filter(folder => folder !== 'img')
+        .flatMap(folder =>
+            Object.keys(zettel[folder]).map(filename => ({folder, filename, lastModified: zettel[folder][filename].lastModified,}))
+        );
 
-    let results = [...files]
-        .filter(file => !ignoredDirs.includes(file.folder)) // Exclude files from ignored directories
-        .sort((a, b) => b.lastModified - a.lastModified) // Sort the remaining files
-        .slice(0, 8); // Take the top 8 recent files
-
+    let results = [...searchableFiles]
+        .filter(file => !ignoredDirs.includes(file.folder))
+        .sort((a, b) => b.lastModified - a.lastModified)
+        .slice(0, 8);
     showResults(results);
 }
 
@@ -577,8 +477,14 @@ function filterFiles() {
 
     let results = [];
 
+    let searchableFiles = Object.keys(zettel)
+        .filter(folder => folder !== 'img')
+        .flatMap(folder =>
+            Object.keys(zettel[folder]).map(filename => ({folder, filename, lastModified: zettel[folder][filename].lastModified,}))
+        );
+
     // Levenshtein distance
-    files.forEach(({filename, folder}) => {
+    searchableFiles.forEach(({filename, folder}) => {
         const potentialMatch = filename.replace(/\.md$/, "");
         let similarityScore = similarity(search, potentialMatch);
         if (similarityScore >= 70) {
@@ -590,7 +496,7 @@ function filterFiles() {
     });
 
     // Substring
-    files.forEach(({filename, folder}) => {
+    searchableFiles.forEach(({filename, folder}) => {
         const potentialMatch = filename.replace(/\.md$/, "");
         const isSubstringMatch = potentialMatch.toLowerCase().includes(search.toLowerCase());
         if (!isSubstringMatch) {
---
+++ web/lib/similarity.js
@@ -0,0 +1,101 @@
+// Similarity between to string from 0 to 100
+function similarity(s1, s2) {
+    const distance = levenshtein(s1.toLowerCase(), s2.toLowerCase());
+    const maxLength = Math.max(s1.length, s2.length);
+
+    if (maxLength === 0) return 100;
+
+    const similarity = (1 - distance / maxLength) * 100;
+
+    return similarity.toFixed(2);
+}
+
+function levenshtein(s1, s2) {
+    if (s1 === s2) {
+        return 0;
+    }
+    var n = s1.length, m = s2.length;
+    if (n === 0 || m === 0) {
+        return n + m;
+    }
+    var x = 0, y, a, b, c, d, g, h, k;
+    var p = new Array(n);
+    for (y = 0; y < n;) {
+        p[y] = ++y;
+    }
+
+    for (; (x + 3) < m; x += 4) {
+        var e1 = s2.charCodeAt(x);
+        var e2 = s2.charCodeAt(x + 1);
+        var e3 = s2.charCodeAt(x + 2);
+        var e4 = s2.charCodeAt(x + 3);
+        c = x;
+        b = x + 1;
+        d = x + 2;
+        g = x + 3;
+        h = x + 4;
+        for (y = 0; y < n; y++) {
+            k = s1.charCodeAt(y);
+            a = p[y];
+            if (a < c || b < c) {
+                c = (a > b ? b + 1 : a + 1);
+            } else {
+                if (e1 !== k) {
+                    c++;
+                }
+            }
+
+            if (c < b || d < b) {
+                b = (c > d ? d + 1 : c + 1);
+            } else {
+                if (e2 !== k) {
+                    b++;
+                }
+            }
+
+            if (b < d || g < d) {
+                d = (b > g ? g + 1 : b + 1);
+            } else {
+                if (e3 !== k) {
+                    d++;
+                }
+            }
+
+            if (d < g || h < g) {
+                g = (d > h ? h + 1 : d + 1);
+            } else {
+                if (e4 !== k) {
+                    g++;
+                }
+            }
+            p[y] = h = g;
+            g = d;
+            d = b;
+            b = c;
+            c = a;
+        }
+    }
+
+    for (; x < m;) {
+        var e = s2.charCodeAt(x);
+        c = x;
+        d = ++x;
+        for (y = 0; y < n; y++) {
+            a = p[y];
+            if (a < c || d < c) {
+                d = (a > d ? d + 1 : a + 1);
+            } else {
+                if (e !== s1.charCodeAt(y)) {
+                    d = c + 1;
+                } else {
+                    d = c;
+                }
+            }
+            p[y] = d;
+            c = a;
+        }
+        h = d;
+    }
+
+    return h;
+}

6da8dfaf8bdbcd84bf692ee2fe86798d2aea04e2
Artem Zakirullin <artemzr@gmail.com>
2026-04-21 11:24:21 +0300

    fix renamed to inbox.css

--- web/offline.js
+++ web/offline.js
@@ -15,7 +15,7 @@ const urlsToCache = [
     '/lib/hypermd.css',
     '/lib/theme-light.css',
     '/lib/theme-dark.css',
-    '/chat.css',
+    '/inbox.css',
     '/lib/sidebar.js',
     '/lib/codemirror.js',
     '/lib/core.js',
